c6696731614246f5b2fc68340d9708fa771a74be
hmarskiyae <a.khmarskiy@ventra.ru>
2023-01-18 13:40:14 +0200

    first commit

---
+++ bot.py
@@ -0,0 +1,64 @@
+import os
+import logging
+from aiogram import Bot
+from aiogram.dispatcher import Dispatcher
+from aiogram.utils.executor import start_webhook
+from aiogram import Bot, types
+
+import messages
+from random import choice
+
+TOKEN = os.getenv('BOT_TOKEN')
+HEROKU_APP_NAME = os.getenv('HEROKU_APP_NAME')
+bot = Bot(token=TOKEN)
+dp = Dispatcher(bot)
+
+# webhook settings
+WEBHOOK_HOST = f'https://{HEROKU_APP_NAME}.herokuapp.com'
+WEBHOOK_PATH = f'/webhook/{TOKEN}'
+WEBHOOK_URL = f'{WEBHOOK_HOST}{WEBHOOK_PATH}'
+
+# webserver settings
+WEBAPP_HOST = '0.0.0.0'
+WEBAPP_PORT = os.getenv('PORT', default=8000)
+
+
+# @bot.message_handler(commands=['start', 'hello'])
+# def send_welcome(message):
+#     bot.reply_to(message, messages.WELCOME_MESSAGE)
+#
+#
+# @bot.message_handler(commands=['report'])
+# def generate_report(message):
+#     bot.reply_to(message, "тут будет отчет")
+#
+#
+# @bot.message_handler(func=lambda msg: True)
+# def echo_all(message):
+#     bot.reply_to(message, choice(messages.REPLY_MESSAGE))
+
+async def on_startup(dispatcher):
+    await bot.set_webhook(WEBHOOK_URL, drop_pending_updates=True)
+
+
+async def on_shutdown(dispatcher):
+    await bot.delete_webhook()
+
+
+@dp.message_handler()
+async def echo(message: types.Message):
+    await message.answer(message.text)
+
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
+    start_webhook(
+        dispatcher=dp,
+        webhook_path=WEBHOOK_PATH,
+        skip_updates=True,
+        on_startup=on_startup,
+        on_shutdown=on_shutdown,
+        host=WEBAPP_HOST,
+        port=WEBAPP_PORT,
+    )
+
